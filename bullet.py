3ec1dc9314786239aff568d0d75e17c1287ceff0
m275442 <m275442@usna.edu>
2024-11-20 08:28:01 -0500

    Added bullet class

--- bullet.py
+++ bullet.py
@@ -12,3 +12,19 @@ class Bullet(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         # place the bullet
         self.rect.center = (self.x,self.y)
+        print(screen)
+        self.screen_rect = screen.get_rect()
+
+    def update(self):
+        dx = self.speed * cos(radians(self.theta))
+        dy = self.speed * sin(radians(self.theta))
+        
+        self.x += dx
+        self.y -= dy
+        # update the rect
+        self.rect.center = (self.x,self.y)
+
+        # check if the bullet is inside the screen
+        if not self.screen_rect.contains(self.rect):
+            # remove the bullet
+            self.kill()
